25d6e2d60b7df678670ec1fbb160557e81fe0af6
darake <adamson.gert@hotmail.com>
2019-11-11 21:52:23 +0200

    2.10 - 2.12

--- viikko2/Verkkokauppa1/src/main/java/ohtu/verkkokauppa/Varasto.java
+++ viikko2/Verkkokauppa1/src/main/java/ohtu/verkkokauppa/Varasto.java
@@ -1,24 +1,19 @@
 package ohtu.verkkokauppa;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
 import java.util.*;
 
+@Component
 public class Varasto implements VarastoRajapinta {
-
-    private static Varasto instanssi;
-
-    public static Varasto getInstance() {
-        if (instanssi == null) {
-            instanssi = new Varasto();
-        }
-
-        return instanssi;
-    }
     
-    private Kirjanpito kirjanpito;
+    private KirjanpitoRajapinta kirjanpito;
     private HashMap<Tuote, Integer> saldot;  
-    
-    private Varasto() {
-        kirjanpito = Kirjanpito.getInstance();
+
+    @Autowired
+    public Varasto(KirjanpitoRajapinta kirjanpito) {
+        this.kirjanpito = kirjanpito;
         saldot = new HashMap<Tuote, Integer>();
         alustaTuotteet();
     }
