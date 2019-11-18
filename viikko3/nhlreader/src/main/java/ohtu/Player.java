8d0bdd0128760ccab05dd4445266097454755e83
darake <adamson.gert@hotmail.com>
2019-11-18 11:31:36 +0200

    3.1

---
+++ viikko3/nhlreader/src/main/java/ohtu/Player.java
@@ -0,0 +1,47 @@
+
+package ohtu;
+
+public class Player {
+    private String name;
+    private String team;
+    private int goals;
+    private int assists;
+
+    public String getTeam() {
+        return team;
+    }
+
+    public void setTeam(String team) {
+        this.team = team;
+    }
+
+    public int getGoals() {
+        return goals;
+    }
+
+    public int getAssists() {
+        return assists;
+    }
+
+    public void setAssists(int assists) {
+        this.assists = assists;
+    }
+
+    public void setGoals(int goals) {
+        this.goals = goals;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public String toString() {
+        return name + " team " + team + " goals " + goals + " assists " + assists;
+    }
+      
+}
